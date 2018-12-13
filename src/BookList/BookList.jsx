f112d13c30d6e54fbb8f84931e51a4b93da0c4ef
minhpham <minh.pham@dhigroupinc.com>
2018-12-13 17:03:39 -0600

    books

---
+++ src/BookList/BookList.jsx
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import * as axios from 'axios';
+import './BookList.css';
+const url = 'http://localhost:3004/books';
+
+export class BookList extends Component {
+  constructor() {
+    super();
+    this.state = { listView: true, books: [] };
+    axios.get(url).then(response => {
+      this.setState({ books: response.data });
+    });
+  }
+  listView = () => {
+    this.setState({ listView: true });
+  };
+  getFirst100Words = (words)=>{
+      console.log(words);
+      let wordTokens = words.split(' ');
+      if(wordTokens.length > 30){
+          return wordTokens.slice(0,30).join (' ')+'...';
+      }
+      return words;
+  }
+  gridView = () => {
+    this.setState({ listView: false });
+  };
+  render() {
+    console.log(this.state.books);
+    let renderedBookList = this.state.books.map((book, id) => {
+      return (
+        <div
+          className={`item col-xs-4 col-lg-4 ${
+            this.state.listView ? 'list-group-item' : 'grid-group-item'
+          }`}
+          key={id}
+        >
+          <div className="thumbnail card">
+            <div className="img-event">
+              <img
+                className="group list-group-image img-fluid"
+                src={book.image_url}
+                alt=""
+              />
+            </div>
+            <div className="caption card-body">
+              <h4 className="group card-title inner list-group-item-heading">
+                {book.title}
+              </h4>
+              <p className="group inner list-group-item-text">
+                Author:
+                <span>
+                  {book.authors.map(author => author.name).join(', ')}
+                </span>
+              </p>
+              <p className="group inner list-group-item-text">
+                {this.getFirst100Words(book.description)}
+              </p>
+              <div className="row">
+                <div className="col-xs-12 col-md-6">
+                  <a className="btn btn-success" href="/">
+                    Get Info
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+    return (
+      <div>
+        <hr />
+        <hr />
+        <div className="container">
+          <div className="row">
+            <h2>Our current bookstore</h2>
+          </div>
+          <div className="row">
+            <div className="col-lg-12 my-3">
+              <div className="pull-right">
+                <div className="btn-group">
+                  <button
+                    className="btn btn-info"
+                    id="list"
+                    onClick={this.listView}
+                  >
+                    List View
+                  </button>
+                  <button
+                    className="btn btn-danger"
+                    id="grid"
+                    onClick={this.gridView}
+                  >
+                    Grid View
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="products" className="row view-group">
+            {renderedBookList}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- src/Common/NavBar.jsx
+++ src/Common/NavBar.jsx
@@ -2,58 +2,60 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Main } from '../Main/Main';
 import { About } from '../About/About';
+import { BookList } from '../BookList/BookList';
 export class NavBar extends Component {
   render() {
     return (
       <Router>
-          <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-              <div className="container">
-                <a className="navbar-brand" href="index#">
-                  Bloccit
-                </a>
-                <button
-                  className="navbar-toggler"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#navbarResponsive"
-                  aria-controls="navbarResponsive"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span className="navbar-toggler-icon" />
-                </button>
-                <div className="collapse navbar-collapse" id="navbarResponsive">
-                  <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                      {/* <a className="nav-link" href="/">
-                    Home
-                  </a> */}
-                      <Link className="nav-link" to="/">
-                        Home
-                      </Link>
-                      <span className="sr-only">(current)</span>
-                    </li>
-                    <li className="nav-item">
-                      {/* <a className="nav-link" href="/about">
-                    About Us
-                  </a> */}
-                      <Link className="nav-link" to="/about">
-                        About Us
-                      </Link>
-                      <span className="sr-only">(current)</span>
-                    </li>
-                  </ul>
-                </div>
+        <div>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div className="container">
+              <a className="navbar-brand" href="index#">
+                Bloccit
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarResponsive"
+                aria-controls="navbarResponsive"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon" />
+              </button>
+              <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/">
+                      Home
+                    </Link>
+                    <span className="sr-only">(current)</span>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/books">
+                      Books
+                    </Link>
+                    <span className="sr-only">(current)</span>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/about">
+                      About Us
+                    </Link>
+                    <span className="sr-only">(current)</span>
+                  </li>
+                </ul>
               </div>
-            </nav>
+            </div>
+          </nav>
 
-            <hr />
+          <hr />
 
-            <Route exact path="/" component={Main} />
-            <Route path="/about" component={About} />
-          </div>
-        </Router>
+          <Route exact path="/" component={Main} />
+          <Route path="/about" component={About} />
+          <Route path="/books" component={BookList} />
+        </div>
+      </Router>
     );
   }
 }
