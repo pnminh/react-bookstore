a5413de5af7ce0ce1cbc08e00c9e6279b43337e2
minhpham <minh.pham@dhigroupinc.com>
2018-12-29 19:30:39 -0600

    fix client folder location

--- src/app.js
+++ src/app.js
@@ -9,10 +9,10 @@ routeConfig.init(app);
 //host react on node server
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     });
   }
 module.exports = app;
