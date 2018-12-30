00659e92aff16a6915cac48d138f642097b34893
minhpham <minh.pham@dhigroupinc.com>
2018-12-29 18:50:29 -0600

    host react on node server

--- src/app.js
+++ src/app.js
@@ -6,5 +6,13 @@ const routeConfig = require('./config/route-config');
 
 appConfig.init(app, express);
 routeConfig.init(app);
-
+//host react on node server
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
 module.exports = app;
