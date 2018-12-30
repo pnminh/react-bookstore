e96b6df972b9bddedb3fa3a57e975336652f5602
minhpham <minh.pham@dhigroupinc.com>
2018-12-29 19:17:27 -0600

    remove static route

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,12 +2,8 @@
 
 module.exports = {
     init(app){
-      const staticRoutes = require("../routes/static");
       const bookRoutes = require("../routes/books");
-
-      app.use(staticRoutes);
-      app.use(bookRoutes);
-      
+      app.use(bookRoutes);   
     }
 
     
